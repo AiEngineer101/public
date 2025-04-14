539598002e346c6fa0eda36b32fab508aacc2849
AiEngineer101 <admin@aisolv.io>
2025-04-14 11:27:26 -0400

    Update script16.js

--- script16.js
+++ script16.js
@@ -15,7 +15,7 @@
     primaryColor: "#e83e2f",
     initialMessage: "His there! 👋 How can I help you today?",
     position: "bottom-right", // "bottom-right" or "bottom-left"
-    webhookUrl: "https://aisolv.app.n8n.cloud/webhook-test/6cf02e8f-eda1-4eaa-b931-d7e861eeb82f", // New configuration option for webhook URL
+    webhookUrl: "https://aisolv.app.n8n.cloud/webhook/6cf02e8f-eda1-4eaa-b931-d7e861eeb82f", // New configuration option for webhook URL
     sessionId: uuidv4() // Generate a unique session ID
   };
 
